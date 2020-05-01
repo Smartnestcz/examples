e0dd3808a6a269a75b3fe451ba84c2027e8bc2d0
Andres <aososam@gmail.com>
2020-05-01 21:09:37 +0200

    Device code example for all supported devices

---
+++ Devices/airConditioner/airConditioner.ino
@@ -0,0 +1,197 @@
+//#include "WiFi.h"       //Uncomment for ESP32  
+#include <ESP8266WiFi.h> //Comment for ESP-32
+
+#include <PubSubClient.h>// Download and install this library first from: https://www.arduinolibraries.info/libraries/pub-sub-client
+#include <WiFiClient.h>
+
+#define SSID_NAME "Wifi-name"                   // Your Wifi Network name
+#define SSID_PASSWORD "Wifi-password"           // Your Wifi network password
+#define MQTT_BROKER "smartnest.cz"              // Broker host
+#define MQTT_PORT 1883                          // Broker port
+#define MQTT_USERNAME "username"                // Username from Smartnest
+#define MQTT_PASSWORD "password"                // Password from Smartnest (or API key)
+#define MQTT_CLIENT "device-Id"                 // Device Id from smartnest
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+int airConditionerPin=2;
+int sensorpin=A0;
+
+char topic[100];
+char msg[5];
+double temp=25;
+double setpoint=25;
+double lastTimeSent=0;
+
+void startWifi();
+void startMqtt();
+void checkMqtt();
+void mqttLoop();
+int splitTopic(char* topic, char* tokens[] ,int tokensNumber);
+void callback(char* topic, byte* payload, unsigned int length);
+
+void turnOn();
+void turnOff();
+void setSetpoint(double temp);
+void reportTemperature(double temp);
+
+
+void setup(){
+  pinMode(airConditionerPin, OUTPUT);
+  pinMode(sensorpin, INPUT);
+  Serial.begin(115200);
+  startWifi();
+  startMqtt();
+}
+
+void loop(){
+  client.loop();
+  checkMqtt();
+  reportTemperature(temp);
+  
+}
+
+void callback(char* topic, byte* payload, unsigned int length) { //A new message has been received
+
+  int tokensNumber=10;
+  char message[length+1];
+  char *tokens[tokensNumber];
+
+  Serial.print("MQTT Message Received. Topic:");
+  Serial.println(topic);
+  Serial.print("Message:");
+  Serial.println(message);
+
+  splitTopic(topic, tokens, tokensNumber);
+
+  sprintf(message,"%c",(char)payload[0]);
+  for (int i = 1; i < length; i++) {
+    sprintf(message,"%s%c",message,(char)payload[i]);
+  }
+
+  //------------------Decide what to do depending on the topic and message---------------------------------
+
+  if(strcmp(tokens[1],"directive")==0 && strcmp(tokens[2],"powerState")==0){// Turn On or OFF
+
+    if(strcmp(message,"ON")==0){ turnOn(); }
+    else if(strcmp(message,"OFF")==0){ turnOff();}
+  
+  } else if(strcmp(tokens[1],"directive")==0 && strcmp(tokens[2],"setpoint")==0) {// Set Setpoint Temperature
+
+      double value = atof(message);
+      if (isnan(value)){ setpoint = 0; }
+
+      setSetpoint(value);
+    
+      sprintf(topic,"%s/report/setpoint",MQTT_CLIENT);
+      client.publish(topic, message);
+
+  }
+}
+
+void startWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID_NAME, SSID_PASSWORD);
+  Serial.println("Connecting ...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());            
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+  delay(500);  
+}
+
+void startMqtt(){
+
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  client.setCallback(callback);
+
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect(MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD )) {
+      Serial.println("connected");
+    } else {
+      if (client.state()==5){
+
+        Serial.println("Connection not allowed by broker, possible reasons:");
+        Serial.println("- Device is already online. Wait some seconds until it appears offline");
+        Serial.println("- Wrong Username or password. Check credentials");
+        Serial.println("- Client Id does not belong to this username, verify ClientId");
+      
+      } else {
+        Serial.println("Not possible to connect to Broker Error code:");
+        Serial.print(client.state());
+      }
+
+      delay(0x7530);
+    }
+  }
+ 
+  sprintf(topic,"%s/report/online",MQTT_CLIENT); //Reports to the server that device is online
+  client.publish(topic, "true");
+
+  sprintf(topic,"%s/#",MQTT_CLIENT); //Subscribes to all messages send to the device
+  client.subscribe(topic);
+}
+
+int splitTopic(char* topic, char* tokens[],int tokensNumber ){
+  const char s[2] = "/";
+  int pos=0;
+  tokens[0] = strtok(topic, s);
+  while(pos<tokensNumber-1 && tokens[pos] != NULL ) {
+      pos++;
+    tokens[pos] = strtok(NULL, s);
+  }
+  return pos;
+}
+
+void checkMqtt(){
+  if(!client.connected()){ startMqtt(); }
+}
+
+void turnOff(){
+
+  digitalWrite(airConditionerPin, HIGH);
+
+  if(client.connected()){ 
+    sprintf(topic,"%s/report/powerState",MQTT_CLIENT);
+    client.publish(topic, "OFF") ;
+  }
+
+}
+
+void turnOn(){
+    
+  digitalWrite(airConditionerPin, LOW);
+
+  if(client.connected()){ 
+    sprintf(topic,"%s/report/powerState",MQTT_CLIENT);
+    client.publish(topic, "ON") ;
+  }
+
+}
+
+void setSetpoint(double temp){
+
+  Serial.printf("setpoint changed to %3.2f", temp);
+  //Do something with the value, Turn on Heater? turn on AC?
+
+}
+
+void reportTemperature(double temp){
+
+  if(millis()-lastTimeSent>30000 && client.connected()){
+    
+    sprintf(topic,"%s/report/temperature",MQTT_CLIENT);
+    client.publish(topic, "15") ;
+    lastTimeSent = millis();
+
+  }
+
+}
---
+++ Devices/sprinkler/sprinkler.ino
@@ -0,0 +1,186 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>// Download and install this library first from: https://www.arduinolibraries.info/libraries/pub-sub-client
+#include <WiFiClient.h>
+
+#define SSID_NAME "Wifi-name"                   // Your Wifi Network name
+#define SSID_PASSWORD "Wifi-password"           // Your Wifi network password
+#define MQTT_BROKER "smartnest.cz"              // Broker host
+#define MQTT_PORT 1883                          // Broker port
+#define MQTT_USERNAME "username"                // Username from Smartnest
+#define MQTT_PASSWORD "password"                // Password from Smartnest (or API key)
+#define MQTT_CLIENT "device-Id"                 // Device Id from smartnest
+
+WiFiClient espClient;
+PubSubClient client(espClient); 
+int sprinklerPin=0;
+int wateringTime = 5000;                        // Select how long do you want the water pump to be active in ms, 5000 = 5 seconds
+
+void startWifi();
+void startMqtt();
+void checkMqtt();
+int splitTopic(char* topic, char* tokens[] ,int tokensNumber);
+void callback(char* topic, byte* payload, unsigned int length);
+
+void turnOff();
+void turnOn();
+
+void setup() {
+  pinMode(sprinklerPin, OUTPUT);
+  Serial.begin(115200);
+  startWifi();
+  startMqtt();
+}
+
+void loop() {
+  client.loop();
+  checkMqtt();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) { // This function runs when there is a new message in the subscribed topic
+  Serial.print("Topic:");
+  Serial.println(topic);
+
+  // Splits the topic and gets the message
+  int tokensNumber=10;
+  char *tokens[tokensNumber];
+  char message[length+1];
+  splitTopic(topic, tokens, tokensNumber);
+  sprintf(message,"%c",(char)payload[0]);             
+
+  for (int i = 1; i < length; i++) {
+    sprintf(message,"%s%c",message,(char)payload[i]);
+  }
+
+  Serial.print("Message:");
+  Serial.println(message);
+ 
+//------------------ACTIONS HERE---------------------------------
+  char reportChange[100];
+
+  if(strcmp(tokens[1],"directive")==0 && strcmp(tokens[2],"powerState")==0){
+    
+    if(strcmp(message,"ON")==0){
+      turnOn();
+
+    } else if(strcmp(message,"OFF")==0){
+      turnOff();
+      
+    }
+  }
+  
+}
+
+
+void startWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID_NAME, SSID_PASSWORD);
+  Serial.println("Connecting ...");
+  int attempts  = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 10 ) {
+    attempts++;
+    delay(500);
+    Serial.print(".");
+  }
+
+  if(WiFi.status() == WL_CONNECTED){
+
+    Serial.println('\n');
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());             
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+
+  } else {
+
+    Serial.println('\n');
+    Serial.println('I could not connect to the wifi network after 10 attempts \n');
+
+  }
+  
+  delay(500);  
+}
+
+void startMqtt(){
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  client.setCallback(callback);
+
+ while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect(MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD )) {
+      Serial.println("connected");
+
+    } else {
+      if (client.state()==5){
+
+        Serial.println("Connection not allowed by broker, possible reasons:");
+        Serial.println("- Device is already online. Wait some seconds until it appears offline for the broker");
+        Serial.println("- Wrong Username or password. Check credentials");
+        Serial.println("- Client Id does not belong to this username, verify ClientId");
+      
+      } else {
+
+        Serial.println("Not possible to connect to Broker Error code:");
+        Serial.print(client.state());
+      }
+
+      delay(0x7530);
+    }
+  }
+
+  
+  char topic[100];
+  sprintf(topic,"%s/#",MQTT_CLIENT);                     // Sbscribes to the device Topic  device-Id/#
+  client.subscribe(topic);
+
+  sprintf(topic,"%s/report/online",MQTT_CLIENT);         // Reports that the device is online
+  client.publish(topic, "true");
+  delay(200);
+
+}
+
+int splitTopic(char* topic, char* tokens[],int tokensNumber ){
+
+    const char s[2] = "/";
+    int pos=0;
+    tokens[0] = strtok(topic, s);
+
+    while(pos<tokensNumber-1 && tokens[pos] != NULL ) {
+        pos++;
+      tokens[pos] = strtok(NULL, s);
+    }
+
+    return pos;   
+}
+
+
+void checkMqtt(){
+
+  if(!client.connected()){
+    startMqtt();
+  }
+
+}
+
+void turnOff() {
+  Serial.println("Turning Off..");
+  digitalWrite(sprinklerPin, LOW);
+
+  char reportTopic[100];
+  sprintf(reportTopic,"%s/report/powerState",MQTT_CLIENT);
+  client.publish(reportTopic, "OFF");
+
+}
+
+void turnOn() {
+  Serial.println("Turning On");
+  digitalWrite(sprinklerPin, HIGH);
+
+  char reportTopic[100];
+  sprintf(reportTopic,"%s/report/powerState",MQTT_CLIENT);
+  client.publish(reportTopic, "ON");
+  
+  delay(wateringTime);
+  turnOff();
+    
+}
